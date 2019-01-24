8adbaae777e576671743b79b19f3da0355652aec
Justin Brooks <jsbroks@gmail.com>
2019-01-23 21:53:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='imantics',
     version='0.1.0',
@@ -7,8 +10,10 @@ setup(
     url='https://github.com/jsbroks/imantics',
     author='Justin Brooks',
     author_email='jsbroks@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
     install_requires=['numpy', 'opencv-python>=3', 'lxml'],
-    packages=['imantics'],
+    packages=setuptools.find_packages(),
     zip_safe=False
 )
