1a00e8a5b98d0e4704bd4bafa033816aa28773f4
Justin Brooks <jsbroks@gmail.com>
2019-01-23 21:51:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='imantics',
-    version='0.0.0',
+    version='0.1.0',
     description='Python package for managing image annotations',
     url='https://github.com/jsbroks/imantics',
     author='Justin Brooks',
