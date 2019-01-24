b02f7b47f6306d9c5f9daa6e2c317f7b01cc83be
Justin Brooks <jsbroks@gmail.com>
2019-01-23 22:09:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     long_description_content_type="text/markdown",
     license='MIT',
     install_requires=['numpy', 'opencv-python>=3', 'lxml'],
-    packages=setup.find_packages(),
+    packages=['imantics'],
     zip_safe=False
 )
