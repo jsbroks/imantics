50609355413dd7726fdc2f9bc5e572e4e31bc7ff
Robonaut <robonaut@nasa.gov>
2019-02-26 15:09:19 -0600

    Fixed setup to work in 2.7

--- setup.py
+++ setup.py
@@ -1,28 +1,10 @@
 from setuptools import setup
 import subprocess
 
-def run(*popenargs, input=None, check=False, **kwargs)
-    if input is not None:
-        if 'stdin' in kwargs:
-            raise ValueError('stdin and input arguments may not both be used.')
-        kwargs['stdin'] = subprocess.PIPE
-
-    process = subprocess.Popen(*popenargs, **kwargs):
-    try:
-        stdout, stderr = process.communicate(input)
-    except:
-        process.kill()
-        process.wait()
-        raise
-    retcode = process.poll()
-    if check and retcode:
-        raise subprocess.CalledProcessError(
-            retcode, process.args, output=stdout, stderr=stderr)
-    return retcode, stdout, stderr
 
 def get_tag():
-    result = run(["git", "describe", "--abbrev=0", "--tags"], stdout=subprocess.PIPE)
-    return str(result.stdout.decode("utf-8")).strip()[1:]
+    result = subprocess.check_output(["git", "describe", "--abbrev=0", "--tags"])
+    return str(result.strip()[1:])
 
 
 with open("README.md", "r") as fh:
@@ -41,6 +23,6 @@ setup(
     license='MIT',
     install_requires=['numpy', 'opencv-python>=3', 'lxml'],
     packages=['imantics'],
-    python_requires='>=3',
+    python_requires='>=2.7',
     zip_safe=False
 )
